d89cb3a5b9e143281c1c478023965d831d2da935
Phu121091 <nguyentrongphuu@gmail.com>
2023-03-25 10:46:25 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,54 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Layout from "../layout/Layout";
+import { useEffect, useState, useContext } from "react";
+import { useRouter } from "next/router";
+import { createContext } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+const AppContext = createContext();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { asPath } = useRouter();
+  const origin =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "";
+
+  const URL = `${origin}${asPath}`;
+  const [reload, setreload] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    let VNXuser = localStorage.getItem("VNXUser")
+      ? JSON.parse(localStorage.getItem("VNXUser"))
+      : null;
+    if (VNXuser) {
+      setCurrentUser(VNXuser);
+    } else {
+      setCurrentUser(null);
+    }
+  }, [URL]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppContext.Provider
+        value={{
+          currentUser,
+          setCurrentUser,
+          reload,
+          setreload
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppContext.Provider>
+    </QueryClientProvider>
+  );
+}
+
+export function useApppContext() {
+  return useContext(AppContext);
 }
