7a84ea2ab7d2da15600ccff81c0b869a1b3ad927
Phu121091 <nguyentrongphuu@gmail.com>
2023-03-29 08:23:32 +0700

    m

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,17 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;800&display=swap" rel="stylesheet"/>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N9WX3T15YC"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-N9WX3T15YC');
+              `,
+            }}
+          />
       </Head>
       <body>
         <Main />
